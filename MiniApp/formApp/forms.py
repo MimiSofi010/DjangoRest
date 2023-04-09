d34da5f06e6a97488b97c6a37c2dc0011458ae91
MimiSofi010 <mimi.sofi.gonzalez@gmail.com>
2023-04-09 15:38:43 -0400

    adding widgets to form

--- MiniApp/formApp/forms.py
+++ MiniApp/formApp/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
 class UserRegistrationForm(forms.Form):
+    GENDER = [('male', 'MALE'), ('female','FEMALE'), ('other', 'OTHER')]
     firstName=forms.CharField()
     lastName=forms.CharField()
     email=forms.EmailField()
+    comment = forms.CharField(widget=forms.Textarea)
+    gender = forms.CharField(widget=forms.Select(choices=GENDER))
