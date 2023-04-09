d9fa8a92a3950b3660d6831bd8f919459efcbc33
MimiSofi010 <mimi.sofi.gonzalez@gmail.com>
2023-04-09 17:37:55 -0400

    add vustom validation to forms

--- MiniApp/formApp/forms.py
+++ MiniApp/formApp/forms.py
@@ -6,4 +6,18 @@ class UserRegistrationForm(forms.Form):
     lastName=forms.CharField()
     email=forms.EmailField()
     gender = forms.CharField(widget=forms.Select(choices=GENDER), required=False)
+    password = forms.CharField(widget=forms.PasswordInput, required=False)
     comment = forms.CharField(widget=forms.Textarea, required=False)
+    file = forms.FileField(widget=forms.FileInput, required=False)
+    
+    def clean_firstName(self):
+        inputFirstname = self.cleaned_data['firstName']
+        if len(inputFirstname) > 30:
+            raise forms.ValidationError('The max length of First Name is 30 characters')
+        return inputFirstname
+    
+    def clean_lastName(self):
+        inputLastname = self.cleaned_data['lastName']
+        if len(inputLastname) > 30:
+            raise forms.ValidationError('The max length of Last Name is 30 characters')
+        return inputLastname
